c44692bb0958f26fb93bd6119d90e82feedc5b5b
Igor Šula <sulaigor@gmail.com>
2021-07-11 17:09:55 +0200

    Add get parsed game area function and its tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,10 +1,36 @@
 import { STATE_ONE, STATE_TWO } from './states';
-import { getNewState } from './utils';
+import { getNewState, getParsedGameArea } from './utils';
 
-test('return STATE_TWO from STATE_ONE', () => {
-  expect(getNewState(STATE_ONE)).toBe(STATE_TWO);
+describe('getParsedGameArea function:', () => {
+  test('return parsed array state from string', () => {
+    const validState = '1,0;0,1;';
+    const validReturns = [
+      ['1', '0'],
+      ['0', '1'],
+    ];
+
+    expect(getParsedGameArea(validState)).toStrictEqual(validReturns);
+  });
+
+  test('throw error while game state contains not valid cell states', () => {
+    const notValidState = '1,2;0,1;';
+
+    expect(() => getParsedGameArea(notValidState)).toThrowError('Not valid cell states!');
+  });
+
+  test('throw error while game state is now square', () => {
+    const notValidState = '1,0;';
+
+    expect(() => getParsedGameArea(notValidState)).toThrowError('Not valid game state!');
+  });
 });
 
-test('return STATE_ONE from STATE_TWO', () => {
-  expect(getNewState(STATE_TWO)).toBe(STATE_ONE);
+describe('getNewState function:', () => {
+  test('return STATE_TWO from STATE_ONE', () => {
+    expect(getNewState(STATE_ONE)).toBe(STATE_TWO);
+  });
+
+  test('return STATE_ONE from STATE_TWO', () => {
+    expect(getNewState(STATE_TWO)).toBe(STATE_ONE);
+  });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,31 @@
+enum CellStates {
+  LIVE = '1',
+  DEAD = '0',
+}
+
+type GameAreaType = Array<Array<CellStates>>;
+
+export const getParsedGameArea = (gameState: string): GameAreaType => {
+  const parsedState = gameState.split(';').reduce((acc: GameAreaType, row) => {
+    if (row) {
+      acc.push(row.split(',') as Array<CellStates>);
+    }
+    return acc;
+  }, []);
+
+  for (const rowArr of parsedState) {
+    if (rowArr.some((cell) => cell !== CellStates.LIVE && cell !== CellStates.DEAD)) {
+      throw new Error('Not valid cell states!');
+    }
+
+    if (rowArr.length !== parsedState.length) {
+      throw new Error('Not valid game state!');
+    }
+  }
+
+  return parsedState as GameAreaType;
+};
+
 export const getNewState = (initState: string): string => {
   return '';
 };
